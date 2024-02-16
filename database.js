18cb206a4cde7008fa120f406538fccddef21a11
julesss-coder <julesss@posteo.de>
2024-02-16 17:48:20 +0100

    Add GET  routes for all todos and single todos

---
+++ database.js
@@ -0,0 +1,41 @@
+const sqlite = require('sqlite3');
+
+let database = new sqlite.Database('todos.db', error => {
+    if (error) {
+        throw error;
+    } 
+
+    console.log("Connected to database todos.db.");
+
+    database.run(
+        `CREATE TABLE IF NOT EXISTS todos (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            description TEXT,
+            created INTEGER,
+            updated INTEGER
+        )`,
+        (error) => {
+            console.log(error);
+            if (error) return;
+            
+            let insert = "INSERT INTO todos (name, description, created, updated) VALUES (?,?,?,?)";
+
+            database.run(insert, 
+                ["Einkaufen gehen",
+                "Mineralwasser, Bier",
+                Date.now(),
+                Date.now()
+            ]);
+
+            database.run(insert, 
+                ["Sport",
+                "Heute eine Stunde Workout",
+                Date.now(),
+                Date.now()
+            ]);
+        }
+    )
+});
+
+module.exports = database;
--- server.js
+++ server.js
@@ -1,20 +1,58 @@
 const express = require('express');
+const morgan = require('morgan');
+
 const app = express();
 const port = 3000;
+const database = require('./database');
+
+// Middleware
+app.use(morgan('dev'));
 
 app.listen(port, () => {
   console.log('App is listening on port ', port);
 });
 
+// -------- Create rotues -----
+
+// Home page route
 app.get("/", (request, response) => {
   response.send("Request received");
 });
 
+// Route to get all todos
 app.get("/todos", (request, response) => {
-  response.send({
-    id: 1,
-    todo: "shower"
+  database.all(`SELECT * FROM todos`, params = [], (error, rows) => {
+    if (error) {
+      response.status(404).send({
+        message: 'Status code 404 Resource not found'
+      });
+      return;
+    }
+    
+    response.status(200).send({
+      message: "Status code 200 OK", 
+      data: rows
+    });
   });
+});
+
+// Route to get a single todo by id
+app.get("/todos/:id", (request, response) => {
+  console.log("request.params: ", request.params);
+  database.get('SELECT * FROM todos WHERE ID = ?', [request.params.id], (error, row) => {
+    if (error) {
+      response.status(404).send({
+        message: 'status code 404 not found'
+      });
+      return;
+    }
+
+    response.status(200).send({
+      message: 'status code 200',
+      data: row
+    });
+  });
+  
 });
 
 app.post("/new-todo", (request, response, next) => {
